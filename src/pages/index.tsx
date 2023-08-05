2bb5c22f92acee656645674c5f58e87850c7d987
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-08-06 02:43:29 +0700

    change page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import MainCard from '@/components/main-card';
 
 import classes from './home.module.css';
@@ -5,6 +6,9 @@ import classes from './home.module.css';
 export default function Home() {
   return (
     <div className={classes.home}>
+      <Head>
+        <title>Export to Google Sheet</title>
+      </Head>
       <div className={classes.content}>
         <h1>Output/Export to Google Sheets</h1>
         <MainCard />
